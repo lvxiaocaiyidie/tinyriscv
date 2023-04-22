9c68385dab03bdac06a75ca7dfa8a99e3ed23352
lvxiaocaiyidie <50061304+lvxiaocaiyidie@users.noreply.github.com>
2023-04-22 08:55:42 +0800

    啥也没改啊 加了一点if_id的解释

--- rtl/core/if_id.v
+++ rtl/core/if_id.v
@@ -1,3 +1,4 @@
+<<<<<<< HEAD
  /*                                                                      
  Copyright 2019 Blue Liang, liangkangnan@163.com
                                                                          
@@ -16,7 +17,7 @@
 
 `include "defines.v"
 
-// 将指令向译码模块传递
+// 将指令向译码模块传递 引入这个模块 当有中断时把传入译码模块的指令变成INST_NOP 指令地址ZeroWord 外设中断输出信号INT_NONE
 module if_id(
 
     input wire clk,
@@ -50,3 +51,57 @@ module if_id(
     assign int_flag_o = int_flag;
 
 endmodule
+=======
+ /*                                                                      
+ Copyright 2019 Blue Liang, liangkangnan@163.com
+                                                                         
+ Licensed under the Apache License, Version 2.0 (the "License");         
+ you may not use this file except in compliance with the License.        
+ You may obtain a copy of the License at                                 
+                                                                         
+     http://www.apache.org/licenses/LICENSE-2.0                          
+                                                                         
+ Unless required by applicable law or agreed to in writing, software    
+ distributed under the License is distributed on an "AS IS" BASIS,       
+ WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ See the License for the specific language governing permissions and     
+ limitations under the License.                                          
+ */
+
+`include "defines.v"
+// 这一部分根据中断信号的情况对指令进行处理，如果有中断信号，那么就将中断信号传递给译码模块，否则将指令传递给译码模块  这里调用了gen_diff.v中的module 是时序逻辑
+// 将指令向译码模块传递 
+module if_id(
+
+    input wire clk,
+    input wire rst,
+
+    input wire[`InstBus] inst_i,            // 指令内容
+    input wire[`InstAddrBus] inst_addr_i,   // 指令地址
+
+    input wire[`Hold_Flag_Bus] hold_flag_i, // 流水线暂停标志
+
+    input wire[`INT_BUS] int_flag_i,        // 外设中断输入信号
+    output wire[`INT_BUS] int_flag_o,   
+
+    output wire[`InstBus] inst_o,           // 指令内容
+    output wire[`InstAddrBus] inst_addr_o   // 指令地址
+
+    );
+
+    wire hold_en = (hold_flag_i >= `Hold_If);
+
+    wire[`InstBus] inst;
+    gen_pipe_dff #(32) inst_ff(clk, rst, hold_en, `INST_NOP, inst_i, inst);
+    assign inst_o = inst;
+
+    wire[`InstAddrBus] inst_addr;
+    gen_pipe_dff #(32) inst_addr_ff(clk, rst, hold_en, `ZeroWord, inst_addr_i, inst_addr);
+    assign inst_addr_o = inst_addr;
+
+    wire[`INT_BUS] int_flag;
+    gen_pipe_dff #(8) int_ff(clk, rst, hold_en, `INT_NONE, int_flag_i, int_flag);
+    assign int_flag_o = int_flag;
+
+endmodule
+>>>>>>> 7dde846c5f399d5a5079cb70108625763649373e
--- rtl/core/pc_reg.v
+++ rtl/core/pc_reg.v
@@ -1,3 +1,4 @@
+<<<<<<< HEAD
  /*                                                                      
  Copyright 2019 Blue Liang, liangkangnan@163.com
                                                                          
@@ -49,3 +50,56 @@ module pc_reg(
     end
 
 endmodule
+=======
+ /*                                                                      
+ Copyright 2019 Blue Liang, liangkangnan@163.com
+                                                                         
+ Licensed under the Apache License, Version 2.0 (the "License");         
+ you may not use this file except in compliance with the License.        
+ You may obtain a copy of the License at                                 
+                                                                         
+     http://www.apache.org/licenses/LICENSE-2.0                          
+                                                                         
+ Unless required by applicable law or agreed to in writing, software    
+ distributed under the License is distributed on an "AS IS" BASIS,       
+ WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ See the License for the specific language governing permissions and     
+ limitations under the License.                                          
+ */
+
+`include "defines.v"
+
+// PC寄存器模块  产生PC指针传到riscv core再经过总线去存储器中取指令
+module pc_reg(
+
+    input wire clk,
+    input wire rst,
+
+    input wire jump_flag_i,                 // 跳转标志
+    input wire[`InstAddrBus] jump_addr_i,   // 跳转地址
+    input wire[`Hold_Flag_Bus] hold_flag_i, // 流水线暂停标志
+    input wire jtag_reset_flag_i,           // 复位标志
+
+    output reg[`InstAddrBus] pc_o           // PC指针
+
+    );
+
+
+    always @ (posedge clk) begin  //这是时序逻辑 
+        // 复位
+        if (rst == `RstEnable || jtag_reset_flag_i == 1'b1) begin
+            pc_o <= `CpuResetAddr;
+        // 跳转
+        end else if (jump_flag_i == `JumpEnable) begin
+            pc_o <= jump_addr_i;
+        // 暂停
+        end else if (hold_flag_i >= `Hold_Pc) begin
+            pc_o <= pc_o;
+        // 地址加4
+        end else begin
+            pc_o <= pc_o + 4'h4;
+        end
+    end
+
+endmodule
+>>>>>>> 7dde846c5f399d5a5079cb70108625763649373e
